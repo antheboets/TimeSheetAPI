c3313f68da7e723550a973c9ab27ac9a1c87244c
antheboets <anthe.boets@gmail.com>
2019-03-11 01:08:01 +0100

    test

--- TimeSheetAPI/Startup.cs
+++ TimeSheetAPI/Startup.cs
@@ -35,8 +35,8 @@ namespace TimeSheetAPI
             }));
 
             services.AddMvc();
-            string connectionString = Configuration.GetConnectionString("TimeSheetDatabase");
-            //string connectionString = "Server = tcp:ehbpmagroup6.database.windows.net,1433; Initial Catalog = EhbPmaGroup6; Persist Security Info = False; User ID = anthe; Password = YLHNRZ[6cMt[8*NV; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+            //string connectionString = Configuration.GetConnectionString("TimeSheetDatabase");
+            string connectionString = "Server = tcp:ehbpmagroup6.database.windows.net,1433; Initial Catalog = EhbPmaGroup6; Persist Security Info = False; User ID = anthe; Password = YLHNRZ[6cMt[8*NV; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
             services.AddDbContext<TimeSheetContext>(options => options.UseSqlServer(connectionString));
         }
 
