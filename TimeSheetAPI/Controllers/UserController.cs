926bec99eb6132e418156a6c3311d80d84e8d017
antheboets <anthe.boets@gmail.com>
2019-03-11 07:54:16 +0100

    test

--- TimeSheetAPI/Controllers/UserController.cs
+++ TimeSheetAPI/Controllers/UserController.cs
@@ -22,6 +22,21 @@ namespace TimeSheetAPI.Controllers
         [HttpPost("Login")]
         public Dto.User Login([FromBody] Dto.User input)
         {
+            if (input.Email == "anthe.boets@student.ehb.be" && input.Password == "123")
+            {
+                ICollection<Dto.Log> Logs = new List<Dto.Log>();
+                Logs.Add(new Dto.Log { Id = 2, Start = new DateTime().AddMilliseconds(1483254000), Stop = new DateTime().AddMilliseconds(1483270200), Description = "coding-frontend" });
+                Logs.Add(new Dto.Log { Id = 4, Start = new DateTime().AddMilliseconds(1483342200), Stop = new DateTime().AddMilliseconds(1483363800), Description = "coding-backend" });
+                Logs.Add(new Dto.Log { Id = 5, Start = new DateTime().AddMilliseconds(1483428600), Stop = new DateTime().AddMilliseconds(1483457400), Description = "coding-database" });
+                return new Dto.User { Id = 1, Name = "anthe", Email = input.Email, Password = input.Password, Logs = Logs };
+            }
+            if (input.Email == "anthe2.boets@student.ehb.be" && input.Password == "123")
+            {
+                ICollection<Dto.Log> Logs = new List<Dto.Log>();
+                Logs.Add(new Dto.Log { Id = 8, Start = new DateTime().AddMilliseconds(1483255800), Stop = new DateTime().AddMilliseconds(1483277400), Description = "coding html" });
+                Logs.Add(new Dto.Log { Id = 10, Start = new DateTime().AddMilliseconds(1483342200), Stop = new DateTime().AddMilliseconds(1483360200), Description = "coding js" });
+                return new Dto.User { Id = 2, Name = "anthe2", Email = input.Email, Password = input.Password, Logs = Logs };
+            }
             /*
             Models.User User = timeSheetContext.User.Single(x => x.Email == input.Email && x.Password == input.Password);
             ICollection<Dto.Log> Logs = new List<Dto.Log>();
@@ -32,14 +47,13 @@ namespace TimeSheetAPI.Controllers
             
             return new Dto.User { Id = User.Id, Name = User.Name, Email = User.Email, Password = User.Password, Logs = Logs };
             */
-            return input;
+            return new Dto.User();
         }
 
         [HttpPost("Get")]
         public Dto.User GetById([FromBody] Dto.User input)
         {
 
-            Models.User User = timeSheetContext.User.First();
             //Models.User User = timeSheetContext.User.Where(x => x.Id == input.Id);
             /*
             ICollection<Dto.Log> Logs = new List<Dto.Log>();
@@ -50,7 +64,23 @@ namespace TimeSheetAPI.Controllers
             
             return new Dto.User { Id=User.Id, Name=User.Name, Email=User.Email, Password=User.Password, Logs=Logs };
             */
-            return new Dto.User { Id = User.Id};
+            if (input.Id == 1)
+            {
+                ICollection<Dto.Log> Logs = new List<Dto.Log>();
+                Logs.Add(new Dto.Log { Id = 2, Start = new DateTime().AddMilliseconds(1483254000), Stop = new DateTime().AddMilliseconds(1483270200), Description = "coding-frontend" });
+                Logs.Add(new Dto.Log { Id = 4, Start = new DateTime().AddMilliseconds(1483342200), Stop = new DateTime().AddMilliseconds(1483363800), Description = "coding-backend" });
+                Logs.Add(new Dto.Log { Id = 5, Start = new DateTime().AddMilliseconds(1483428600), Stop = new DateTime().AddMilliseconds(1483457400), Description = "coding-database" });
+                return new Dto.User { Id = 1, Name = "anthe", Email = input.Email, Password = input.Password, Logs = Logs };
+            }
+            if (input.Id == 2)
+            {
+                ICollection<Dto.Log> Logs = new List<Dto.Log>();
+                Logs.Add(new Dto.Log { Id = 8, Start = new DateTime().AddMilliseconds(1483255800), Stop = new DateTime().AddMilliseconds(1483277400), Description = "coding html" });
+                Logs.Add(new Dto.Log { Id = 10, Start = new DateTime().AddMilliseconds(1483342200), Stop = new DateTime().AddMilliseconds(1483360200), Description = "coding js" });
+                return new Dto.User { Id = 2, Name = "anthe2", Email = input.Email, Password = input.Password, Logs = Logs };
+            }
+            return new Dto.User();
+       
         }
         /*
         [HttpGet]
