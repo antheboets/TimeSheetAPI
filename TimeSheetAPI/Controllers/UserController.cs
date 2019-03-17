a7f647a7a73d3f7a4525116db5f41559c1988c18
antheboets <anthe.boets@gmail.com>
2019-03-17 21:23:43 +0100

    adding error handeling

--- TimeSheetAPI/Controllers/UserController.cs
+++ TimeSheetAPI/Controllers/UserController.cs
@@ -22,30 +22,46 @@ namespace TimeSheetAPI.Controllers
         [HttpPost("Login")]
         public async Task<Dto.User> Login([FromBody] Dto.User input)
         {
-            
-            Models.User User = await TimeSheetContext.User.Include(x => x.Logs).SingleAsync(x => x.Email == input.Email && x.Password == input.Password);
-            ICollection<Dto.Log> Logs = new List<Dto.Log>();
-            foreach (var log in User.Logs)
+            if (input.Email == "" || input.Password == "")
             {
-                Logs.Add(new Dto.Log { Id = log.Id, Start = log.Start, Stop = log.Stop, Description = log.Description });
+                return new Dto.User();
             }
-
-            return new Dto.User { Id = User.Id, Name = User.Name, Email = User.Email, Password = User.Password, Logs = Logs };
+            Models.User User = await TimeSheetContext.User.Include(x => x.Logs).SingleOrDefaultAsync(x => x.Email == input.Email && x.Password == input.Password);
+            ICollection<Dto.Log> Logs = new List<Dto.Log>();
+            if (User != null)
+            {
+                foreach (var log in User.Logs)
+                {
+                    Logs.Add(new Dto.Log { Id = log.Id, Start = log.Start, Stop = log.Stop, Description = log.Description });
+                }
+                return new Dto.User { Id = User.Id, Name = User.Name, Email = User.Email, Password = User.Password, Logs = Logs };
+            }
+            else
+            {
+                return new Dto.User();
+            }
         }
-
         [HttpPost("Get")]
         public async Task<Dto.User> GetById([FromBody] Dto.User input)
         {
-            
-            Models.User User =  await TimeSheetContext.User.Include(x => x.Logs).SingleAsync(x => x.Id == input.Id);
-            
-            ICollection<Dto.Log> Logs = new List<Dto.Log>();
-            foreach (var log in User.Logs)
+            if (input.Id == 0)
             {
-                Logs.Add(new Dto.Log { Id=log.Id, Start = log.Start, Stop = log.Stop, Description = log.Description});
+                return new Dto.User();
             }
-            
-            return new Dto.User { Id=User.Id, Name=User.Name, Email=User.Email, Password=User.Password, Logs=Logs };
+            Models.User User =  await TimeSheetContext.User.Include(x => x.Logs).SingleAsync(x => x.Id == input.Id);
+            ICollection<Dto.Log> Logs = new List<Dto.Log>();
+            if (User != null)
+            {
+                foreach (var log in User.Logs)
+                {
+                    Logs.Add(new Dto.Log { Id = log.Id, Start = log.Start, Stop = log.Stop, Description = log.Description });
+                }
+                return new Dto.User { Id = User.Id, Name = User.Name, Email = User.Email, Password = User.Password, Logs = Logs };
+            }
+            else
+            {
+                return new Dto.User();
+            }
         }
         [HttpGet("test")]
         public async Task<Dto.User> Test()
