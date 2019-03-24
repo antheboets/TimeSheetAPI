2b6684ea0a811da1cb89f46ab166a77e0f8a1d53
antheboets <anthe.boets@gmail.com>
2019-03-24 12:31:38 +0100

    crud for logs

--- TimeSheetAPI/Controllers/LogController.cs
+++ TimeSheetAPI/Controllers/LogController.cs
@@ -24,21 +24,42 @@ namespace TimeSheetAPI.Controllers
         [HttpPost("Create")]
         public async Task<ActionResult> Create(Dto.LogForCreate log)
         {
-            Models.Log ModelLog = new Models.Log { Start=log.Start, Stop=log.Stop, Description=log.Description, UserId= User.FindFirst(ClaimTypes.NameIdentifier).Value };
+            Models.Log ModelLog = new Models.Log { Start=log.Start, Stop=log.Stop, Description=log.Description, UserId= User.FindFirst(ClaimTypes.NameIdentifier).Value, Activity= new Models.Activity(), Project = new Models.Project() };
             await TimeSheetContext.AddAsync(ModelLog);
+            await TimeSheetContext.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPost("Delete")]
+        public async Task<ActionResult> Delete([FromBody] Dto.LogForDelete log)
+        {
+            Models.Log ModelLog = await TimeSheetContext.Log.Where(x => x.Id == log.Id).Where(x => x.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value).SingleOrDefaultAsync();
+            if (ModelLog == null)
+            {
+                BadRequest();
+            }
+            TimeSheetContext.Remove(ModelLog);
+            await TimeSheetContext.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPost("Update")]
+        public async Task<ActionResult> Update(Dto.LogForUpdate log)
+        {
+            Models.Log ModelLog = new Models.Log { Id=log.Id, Start = log.Start, Stop = log.Stop, Description = log.Description, UserId = log.UserId };
+            TimeSheetContext.Update(ModelLog);
             await TimeSheetContext.SaveChangesAsync();
             return Ok();
         }
         [HttpGet("Get")]
         public async Task<Dto.Log> Get([FromQuery] String Id)
         {
-            /*
-            if ()
+            Models.Log log = await TimeSheetContext.Log.Where(x => x.Id == Id).Where(x => x.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value).FirstOrDefaultAsync();
+            if (log == null)
             {
-
+                return null;
             }
-            */
-            return null;
+            Dto.Log logDto = new Dto.Log{ Id= log.Id, Start = log.Start, Stop= log.Stop, Description = log.Description }; 
+
+            return logDto;
         }
         [HttpGet("Test")]
         public async Task<Dto.Log> Get()
---
+++ TimeSheetAPI/Dto/LogForUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetAPI.Dto
+{
+    public class LogForUpdate
+    {
+        public string Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+        public String Description { get; set; }
+        public string UserId { get; set; }
+    }
+}
