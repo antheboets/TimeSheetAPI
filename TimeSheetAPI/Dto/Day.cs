0695cd9537adcd3fccdd2dc6b6d6465fc38c4d1e
antheboets <44194665+antheboets@users.noreply.github.com>
2019-04-01 12:54:31 +0200

    adding dto day and defaultWorkweek

---
+++ TimeSheetAPI/Dto/Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetAPI.Dto
+{
+    public class Day
+    {
+        public string Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+    }
+}
