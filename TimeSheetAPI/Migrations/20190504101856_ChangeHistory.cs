807e8ed57e3cfd9bbf55ce4069a3b1d63edbc15f
antheboets <anthe.boets@gmail.com>
2019-05-04 12:21:54 +0200

    adding changeHistory to database

---
+++ TimeSheetAPI/Migrations/20190504101856_ChangeHistory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeSheetAPI.Migrations
+{
+    public partial class ChangeHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ChangeHistory",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeHistory",
+                table: "User");
+        }
+    }
+}
